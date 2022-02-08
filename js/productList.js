96ac53deb3e92d129f5b007dbe963317caa72108
Karina Valens <karyvalens@yahoo.com>
2022-02-08 09:13:48 +0100

    calc font apllied

--- js/productList.js
+++ js/productList.js
@@ -17,8 +17,6 @@ function productL(data) {
     /* <template id="myTemplate">
                     <article class="proImg soldOut">
                         <a href="./product.html"> <img src="../images/1163.jpg" alt="T-shirt"></a>
-                        <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
-                        <p>Tshirts | Nike</p>
                         <p>Price: <span> 1595,-</span></p>
                         <a href="./product.html">Read More</a>
                         <div class="discounted">
@@ -37,9 +35,26 @@ function showProduct(product) {
     // 3. Change data
     console.log(product);
     clone.querySelector(".producType").textContent = `${product.articletype} | ${product.brandname}`;
+    clone.querySelector("h3").textContent = `${product.productdisplayname}`;
+    clone.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+    clone.querySelector("img").alt = `https://kea-alt-del.dk/t7/images/webp/1000/${product.displayname}.webp`;
+    clone.querySelector(".price").textContent = `${product.price}`;
+    clone.querySelector(".discounted p:last-child").textContent = `-${product.discount}%`;
+
+    // loop into the products to determind wich has discount or is sold out to add classes
+    if (product.soldout) {
+        clone.querySelector("article").classList.add("soldOut");
+    }
+    if (product.discount) {
+        clone.querySelector("article").classList.add("onSale");
+        clone.querySelector(".discounted p span").textContent = product.price - (product.price * (product.discount / 100));
+    }
+
+    // calculating the discounted price
+
 
     // 4. Choose the parent
-    const parent = document.querySelector("main");
+    const parent = document.querySelector("#container");
     // 5. Append the data
     parent.appendChild(clone);
 }
