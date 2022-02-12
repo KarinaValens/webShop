1be00cec842d3dc28ef0c017122e41a84c557bae
Karina Valens <karyvalens@yahoo.com>
2022-02-12 15:11:10 +0100

    category page dinamic

---
+++ js/category.js
@@ -0,0 +1,27 @@
+/* const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category"); */
+const url = "https://kea-alt-del.dk/t7/api/categories";
+console.log(url);
+
+/* fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => showCategory(data)); */
+
+fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => data.forEach(showCategory));
+
+//populate the page
+function showCategory(category) {
+    console.log(category.category);
+    // 1. Select template
+    const catTemplate = document.querySelector("#categoryTemplate").content;
+    // 2. Make a clone (the true stament in the clone make it include its children)
+    const catClone = catTemplate.cloneNode(true);
+    // 3. Change data
+    catClone.querySelector(".categoryName").textContent = `${category.category}`
+    // 4. Choose a parent
+    const parent = document.querySelector("#container");
+    // 5. Append the data
+    parent.appendChild(catClone);
+}
